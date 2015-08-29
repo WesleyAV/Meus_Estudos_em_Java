e675c775910e714f1dae735da19d70fef0ee5f26
Wesley A.V <wesley.contato@hotmail.com>
2015-08-29 00:08:12 -0300

    Indentação

--- Exercícios/Listas/Exercicios_Aula13_Loiane/src/exer_aula13_loiane/Exer_03.java
+++ Exercícios/Listas/Exercicios_Aula13_Loiane/src/exer_aula13_loiane/Exer_03.java
@@ -10,17 +10,18 @@ import java.util.Scanner;
  * @author Wesley
  */
 public class Exer_03 {
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner (System.in);
-        
+        Scanner scan = new Scanner(System.in);
+
         System.out.println("Digite o primeiro número: ");
         int num1 = scan.nextInt();
-        
+
         System.out.println("Digite o segundo número ");
         int num2 = scan.nextInt();
-        
+
         int soma = num1 + num2;
-        
+
         System.out.println("A soma dos números digitados é: " + soma);
     }
 }
