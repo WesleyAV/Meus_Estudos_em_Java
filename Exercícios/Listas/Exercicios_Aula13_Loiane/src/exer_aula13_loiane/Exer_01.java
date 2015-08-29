7b6adf364d4e196e45b1fe7a13bab512b3a35d5c
Wesley A.V <wesley.contato@hotmail.com>
2015-08-28 23:33:30 -0300

    Lista de exercícios - aula 13 - Loiane Groner

---
+++ Exercícios/Listas/Exercicios_Aula13_Loiane/src/exer_aula13_loiane/Exer_01.java
@@ -0,0 +1,14 @@
+/*
+ * Faça um Programa que mostre a mensagem "Alo mundo" na tela. 
+ */
+package exer_aula13_loiane;
+
+/**
+ *
+ * @author Wesley
+ */
+public class Exer_01 {
+    public static void main(String[] args) {
+        System.out.println("Alô mundo!");
+    }
+}
