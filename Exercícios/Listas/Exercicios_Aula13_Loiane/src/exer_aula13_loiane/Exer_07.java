2c325108b2373cf93c97e01bbd8d26bfaa55c382
Wesley A.V <wesley.contato@hotmail.com>
2015-08-30 23:01:00 -0300

    Exercício 7
    Faça um Programa que calcule a área de um quadrado, em seguida mostre o
    dobro desta área para o usuário.

---
+++ Exercícios/Listas/Exercicios_Aula13_Loiane/src/exer_aula13_loiane/Exer_07.java
@@ -0,0 +1,25 @@
+/*
+ * Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário. 
+ */
+package exer_aula13_loiane;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Wesley
+ */
+public class Exer_07 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Entre com a medida do lado do quadrado: ");
+        double lado = scan.nextDouble();
+
+        double area = lado * lado;
+
+        System.out.println("A área é de: " + (area * 2));
+
+    }
+}
