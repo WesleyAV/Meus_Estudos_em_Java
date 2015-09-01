5a977dc4d5d2d127944b790e95753aa46c68c2ff
Wesley A.V <wesley.contato@hotmail.com>
2015-09-01 10:16:50 -0300

    Exercício 9
    Faça um Programa que peça a temperatura em graus Farenheit,
    transforme e mostre a temperatura em graus Celsius.  o C = (5 * (F-32) /
    9).

---
+++ Exercícios/Listas/Exercicios_Aula13_Loiane/src/exer_aula13_loiane/Exer_09.java
@@ -0,0 +1,26 @@
+/*
+ * Faça um Programa que peça a temperatura em graus Farenheit,
+ transforme e mostre a temperatura em graus Celsius.
+ o C = (5 * (F-32) / 9).
+ */
+package exer_aula13_loiane;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Wesley
+ */
+public class Exer_09 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite a temperatura em graus Farenheit: ");
+        double f = scan.nextDouble();
+
+        double c = (5 * (f - 32) / 9);
+
+        System.out.println("A temperatura de " + f + " graus Farenheit é equivalente a " + c + " graus Celcius.");
+    }
+}
