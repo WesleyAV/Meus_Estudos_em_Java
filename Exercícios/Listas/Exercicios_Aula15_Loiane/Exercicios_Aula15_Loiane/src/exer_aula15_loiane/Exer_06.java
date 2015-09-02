66d0d739cd20f66b47ab525e58dc8b635d9facc1
Wesley A.V <wesley.contato@hotmail.com>
2015-09-02 09:25:35 -0300

    Exercício 06
    Faça um Programa que leia três números e mostre o maior deles.

---
+++ Exercícios/Listas/Exercicios_Aula15_Loiane/Exercicios_Aula15_Loiane/src/exer_aula15_loiane/Exer_06.java
@@ -0,0 +1,35 @@
+/*
+ * Faça um Programa que leia três números e mostre o maior deles.
+ */
+package exer_aula15_loiane;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Wesley
+ */
+public class Exer_06 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        int num1 = scan.nextInt();
+
+        System.out.println("Digite o segundo número: ");
+        int num2 = scan.nextInt();
+
+        System.out.println("Digite o terceiro número: ");
+        int num3 = scan.nextInt();
+
+        if (num1 >= num2 && num1 >= num3) {
+            System.out.println("O primeiro número é o maior deles.");
+        } else if (num2 >= num1 && num2 >= num3) {
+            System.out.println("O segundo número é o maior deles");
+        } else if (num3 >= num1 && num3 >= num2) {
+            System.out.println("O terceiro número é o maior deles");
+        }
+
+    }
+}
