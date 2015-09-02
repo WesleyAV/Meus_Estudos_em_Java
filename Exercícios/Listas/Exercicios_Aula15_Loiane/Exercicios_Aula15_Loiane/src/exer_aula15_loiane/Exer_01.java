026a9b7afdecbd614f566f5beee6c3d6d9dbefc3
Wesley A.V <wesley.contato@hotmail.com>
2015-09-02 01:13:56 -0300

    Exercício 02
    Faça um Programa que peça dois números e imprima o maior deles.

---
+++ Exercícios/Listas/Exercicios_Aula15_Loiane/Exercicios_Aula15_Loiane/src/exer_aula15_loiane/Exer_01.java
@@ -0,0 +1,28 @@
+package exer_aula15_loiane;
+
+import java.util.Scanner;
+
+/*
+ * Faça um Programa que peça dois números e imprima o maior deles. 
+
+/**
+ *
+ * @author Wesley
+ */
+public class Exer_01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Digite o primeiro número: ");
+        int num1 = scan.nextInt();
+        
+        System.out.println("Digite o segundo número: ");
+        int num2 = scan.nextInt();
+        
+        if (num1 > num2){
+            System.out.println("O primeiro número é MAIOR");
+        } else {
+            System.out.println("O segundo número é MAIOR");
+        }
+    }
+}
